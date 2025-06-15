19eb473d7d9594c42b52695d774a6c3b11598b8e
hannanizzati <168669267+hannanizzati@users.noreply.github.com>
2025-06-15 16:45:26 +0900

    fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    sequence = []
+    a, b = 0, 1
+    while len(sequence) < n:
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
+
+# Example: print first 10 Fibonacci numbers
+print(fibonacci(10))
